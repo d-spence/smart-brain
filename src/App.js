ccaa37fd32249e9c0387b76d4daacff7e329fb88
Dan Spencer <dspence180@gmail.com>
2021-02-06 17:33:11 -0600

    Moved clarifai api to backend (smart-brain-api) for security purposes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
-
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -10,10 +8,6 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: '2b82bc44e40e419fb4d8b8e43f3e3a72'
-});
 
 const particlesOptions = {
   particles: {
@@ -112,10 +106,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+      fetch('http://localhost:3001/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          input: this.state.input,
+        })
+      })
+      .then(response => response.json())
       .then((response) => {
         if (response) {
           fetch('http://localhost:3001/image', {
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 function Logo() {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max: 35, speed: 2000 }} style={{ height: 200, width: 200, 'transform-style': 'preserve-3d' }} >
+      <Tilt className="Tilt br2 shadow-2" options={{ max: 35, speed: 2000 }} style={{ height: 200, width: 200, transformStyle: 'preserve-3d' }} >
         <div className="Tilt-inner pa1" style={{paddingTop: '20px', transform: 'translateZ(30px)' }}>
           <img className="w-80" src={brain} alt="brain logo"/>
         </div>
