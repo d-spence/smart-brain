a6b4ffc810a9be900f75f074486ef681270b7423
Dan Spencer <dspence180@gmail.com>
2021-02-06 16:12:04 -0600

    Added parallax tilt effect to logo. Fixed bug with onSubmitRegiser function in Register.js.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,8 +6,8 @@ import './Logo.css';
 function Logo() {
   return (
     <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 30 }} style={{ height: 200, width: 200 }} >
-        <div className="Tilt-inner pa1" style={{paddingTop: '20px'}}>
+      <Tilt className="Tilt br2 shadow-2" options={{ max: 35, speed: 2000 }} style={{ height: 200, width: 200, 'transform-style': 'preserve-3d' }} >
+        <div className="Tilt-inner pa1" style={{paddingTop: '20px', transform: 'translateZ(30px)' }}>
           <img className="w-80" src={brain} alt="brain logo"/>
         </div>
       </Tilt>
