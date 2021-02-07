155c2c005dba76e456a71cb845c96385288b136a
Dan Spencer <dspence180@gmail.com>
2021-02-07 14:30:05 -0600

    Changed fetch from localhost to heroku url.

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/signin', {
+    fetch('https://polar-plateau-76372.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
